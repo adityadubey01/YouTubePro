eff248be4cbc6142f41ea65719a0d7fd8cfca26e
adityadubey01 <adityadubey412@gmail.com>
2023-11-03 23:59:13 +0530

    LiveMessage/User

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,35 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "./utils/chatSlice";
+import { generateRandomName, generateRandomMessage } from "./utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const ChatMessages = useSelector((store) => store.chat.messages);
 
   useEffect(() => {
     const setInter = setInterval(() => {
       dispatch(
-        addMessage({ name: "Redux", message: "Check my live msz...😁" })
+        addMessage({
+          name: generateRandomName(),
+          message: generateRandomMessage(15) + "😁",
+        })
       );
-    }, 1000);
+    }, 1500);
 
     return () => {
       clearInterval(setInter);
     };
   }, []);
   return (
-    <div className="max-h-[540px] overflow-y-scroll">
-      {ChatMessages.map((currentMessage, index) => {
-        return (
-          <ChatMessage
-            key={index}
-            name={currentMessage.name}
-            message={currentMessage.message}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="max-h-[540px] overflow-y-scroll flex flex-col-reverse">
+        {ChatMessages.map((currentMessage, index) => {
+          return (
+            <ChatMessage
+              key={index}
+              name={currentMessage.name}
+              message={currentMessage.message}
+            />
+          );
+        })}
+      </div>
+      <div className="w-full p-2 border border-black">
+        <input
+          className="w-56 pl-1 border border-black"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button
+          className="px-1 ml-2 border border-black bg-green-100"
+          onClick={() => {
+            dispatch(addMessage({ name: "Aditya", message: liveMessage }));
+            setLiveMessage("");
+          }}
+        >
+          Send
+        </button>
+      </div>
+    </>
   );
 };
 
